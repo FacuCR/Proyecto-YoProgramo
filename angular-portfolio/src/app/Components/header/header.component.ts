a71229de897173b82da8007a0f1b68e8a5c8979f
FacuCR <facucastro302@gmail.com>
2022-01-25 23:55:39 -0300

    funciones del header component refactorizado con mejores practicas

--- angular-portfolio/src/app/Components/header/header.component.ts
+++ angular-portfolio/src/app/Components/header/header.component.ts
@@ -30,56 +30,69 @@ export class HeaderComponent implements AfterViewChecked, OnInit {
 
  ngAfterViewChecked() {
 
-  /*
-  * El test de creacion del componente está tratando de obtener el valor de html antes de renderizar por completo.
-  * El setTimeout es lo unico que me esta funcionando para el test de momento.
-  * No se necesita del setTimeout fuera del test, el componente se crea correctamente.
-  * Nota: si no funciona el test aumentar el tiempo de espera.
-  */
-  setTimeout(() => {
-    const heroElement: HTMLElement = this.document.getElementById('hero') as HTMLElement;
-    const aboutElement: HTMLElement = this.document.getElementById('about') as HTMLElement;
-    const habElement: HTMLElement = this.document.getElementById('habilidades') as HTMLElement;
-    const portfolioElement: HTMLElement = this.document.getElementById('portfolio') as HTMLElement;
-    const contactoElement: HTMLElement = this.document.getElementById('contacto') as HTMLElement;
-  
-    this.heroOffset = heroElement.offsetTop;
-    this.aboutOffset = aboutElement.offsetTop;
-    this.habOffset = habElement.offsetTop;
-    this.portfolioOffset = portfolioElement.offsetTop; 
-    this.contactoOffset = contactoElement.offsetTop;
-  }, 0.00001);
+    const heroElement: HTMLElement = this.getElementConId('hero');
+    const aboutElement: HTMLElement = this.getElementConId('about');
+    const habElement: HTMLElement = this.getElementConId('habilidades');
+    const portfolioElement: HTMLElement = this.getElementConId('portfolio');
+    const contactoElement: HTMLElement = this.getElementConId('contacto');
+    
+    /*
+    * El test de creacion del componente está tratando de obtener el valor de html antes de renderizar por completo.
+    * El setTimeout es lo unico que me esta funcionando para el test de momento.
+    * No se necesita del setTimeout fuera del test, el componente se crea correctamente.
+    * Nota: si no funciona el test aumentar el tiempo de espera.
+    */
+    setTimeout(() => {    
+      this.heroOffset = heroElement.offsetTop;
+      this.aboutOffset = aboutElement.offsetTop;
+      this.habOffset = habElement.offsetTop;
+      this.portfolioOffset = portfolioElement.offsetTop; 
+      this.contactoOffset = contactoElement.offsetTop;
+    });
     
   }
 
   /* ---===== Chequear la posicion para cambiar el cursor active del menu horizontal =====--- */
   @HostListener('window:scroll', ['$event'])
   checkOffsetTop(): void {
-    if (window.pageYOffset >= this.heroOffset && window.pageYOffset < this.aboutOffset) {
+    if (this.ifContenidoEntre(this.heroOffset, this.aboutOffset)) {
       this.currentActive = 1;
-    } else if (window.pageYOffset >= this.aboutOffset && window.pageYOffset < this.habOffset) {
+    } 
+    else if (this.ifContenidoEntre(this.aboutOffset, this.habOffset)) {
       this.currentActive = 2;
-    } else if (window.pageYOffset >= this.habOffset && window.pageYOffset < this.portfolioOffset) {
+    } 
+    else if (this.ifContenidoEntre(this.habOffset, this.portfolioOffset)) {
       this.currentActive = 3;
-    } else if (window.pageYOffset >= this.portfolioOffset && window.pageYOffset < this.contactoOffset) {
+    } 
+    else if (this.ifContenidoEntre(this.portfolioOffset, this.contactoOffset)) {
       this.currentActive = 4;
-    } else if (window.pageYOffset >= this.contactoOffset) {
+    } 
+    else if (this.ifContenidoEntre(this.contactoOffset)) {
       this.currentActive = 5;
-    } else {
+    } 
+    else {
       this.currentActive = 0;
     }
   }
   
-  /* ---===== Cambiar el valor de la variable cuando se cambien el tamaño de la ventana =====--- */
   @HostListener('window:resize', ['$event'])
   onResize(event: any) {
     this.screenXl = window.innerWidth >= 1200;
   }
 
-  /* ---===== Cambiar el menu-toggle a menu-active de mobile =====--- */
   activarMobileNav(): void {
     this.menuAbierto = !this.menuAbierto;
     this.document.querySelector('#header').classList.toggle('start-0');
+  }
+
+  getElementConId(elementId: string): HTMLElement {
+    return this.document.getElementById(elementId) as HTMLElement;
+  }
+
+  ifContenidoEntre(elementoDeArriba: number, elementoDeAbajo?: number): boolean {
+    if(elementoDeAbajo) return window.pageYOffset >= elementoDeArriba && window.pageYOffset < elementoDeAbajo;
+
+    return window.pageYOffset >= elementoDeArriba;
   }
 
   /* Posible solucion para la posicion del active para cuando agregue elementos de forma dinamica *
