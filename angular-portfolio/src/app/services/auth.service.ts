5264ae5fcdd38d2bb372117833eb71da7b3e6298
FacuCR <facucastro302@gmail.com>
2022-03-02 16:40:20 -0300

    auth service login

--- angular-portfolio/src/app/services/auth.service.ts
+++ angular-portfolio/src/app/services/auth.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+const AUTH_API = 'http://localhost:8080/api/auth/';
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+};
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  login(email: string, contrasenia: string): Observable<any> {
+    return this.http.post(
+      AUTH_API + 'signin',
+      {
+        email,
+        contrasenia,
+      },
+      httpOptions
+    );
+  }
 }
