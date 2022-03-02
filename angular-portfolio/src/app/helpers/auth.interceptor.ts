883e9bd78633bc3fa1e345fa2bf7ba1f9dd62162
FacuCR <facucastro302@gmail.com>
2022-03-02 17:07:32 -0300

    http interceptor en auth interceptor helpers

--- angular-portfolio/src/app/helpers/auth.interceptor.ts
+++ angular-portfolio/src/app/helpers/auth.interceptor.ts
@@ -1,1 +1,26 @@
-export class AuthInterceptorProviders{}
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { TokenStorageService } from "../services/token-storage.service";
+
+const TOKEN_HEADER_KEY: string = "Authorization"; // para el back spring boot
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor{
+    
+    constructor(private token: TokenStorageService) {}
+
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        let authReq = request;
+        const token = this.token.getToken;
+
+        if(token != null) {
+            authReq = request.clone({ headers: request.headers.set(TOKEN_HEADER_KEY, "Bearer " + token)});
+        }
+        return next.handle(authReq);
+    }
+}
+
+export const AuthInterceptorProviders = [{
+    provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true
+}]
--- angular-portfolio/src/app/services/user.service.ts
+++ angular-portfolio/src/app/services/user.service.ts
@@ -1,9 +1,12 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+const API_URL = 'http://localhost:8080/api/';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 }
