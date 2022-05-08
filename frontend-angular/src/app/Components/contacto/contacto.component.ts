926ad00573e4dc5b33077be442fc7c5b181f51da
FacuCR <facucastro302@gmail.com>
2022-05-08 03:17:10 -0300

    formulario de contacto listo

--- frontend-angular/src/app/Components/contacto/contacto.component.ts
+++ frontend-angular/src/app/Components/contacto/contacto.component.ts
@@ -1,49 +1,87 @@
 import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+} from '@angular/forms';
 import { Validators } from '@angular/forms';
+import { ContactoService } from 'src/app/services/contacto.service';
 
 @Component({
   selector: 'app-contacto',
   templateUrl: './contacto.component.html',
-  styleUrls: ['./contacto.component.css']
+  styleUrls: ['./contacto.component.css'],
 })
 export class ContactoComponent implements OnInit {
-
   formContacto: FormGroup = new FormGroup({});
-  nombre: FormControl = new FormControl("", [Validators.required]);
-  email: FormControl = new FormControl("", [Validators.required, Validators.email]);
-  asunto: FormControl = new FormControl("", [Validators.required]);
-  mensaje: FormControl = new FormControl("", [Validators.required, Validators.maxLength(300)]);
-  antiSpam: FormControl = new FormControl("");
+  nombre: FormControl = new FormControl('', [Validators.required]);
+  email: FormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+  mensaje: FormControl = new FormControl('', [
+    Validators.required,
+    Validators.maxLength(300),
+  ]);
+  antiSpam: FormControl = new FormControl('');
   submitted: boolean = false;
   cargando: boolean = false;
-  mensajeDeRespuesta: string = "";
+  mensajeDeRespuesta: string = '';
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private contactoService: ContactoService
+  ) {
     this.formContacto = this.formBuilder.group({
       nombre: this.nombre,
       email: this.email,
-      asunto: this.asunto,
       mensaje: this.mensaje,
-      antiSpam: this.antiSpam
-    })
-   }
-
-  ngOnInit() {
-    if(this.formContacto.status === 'VALID' && this.antiSpam.value === ''){
-      
-    }
+      antiSpam: this.antiSpam,
+    });
   }
 
-  onSubmit() {}
+  ngOnInit() {}
+
+  onSubmit() {
+    if (this.formContacto.status === 'VALID' && this.antiSpam.value === '') {
+      this.formContacto.disable();
+      const nombre: string = this.formContacto.get('nombre')?.value;
+      const email: string = this.formContacto.get('email')?.value;
+      const mensaje: string = this.formContacto.get('mensaje')?.value;
+      this.cargando = true;
+      this.submitted = false;
+
+      this.mensajeDeRespuesta =
+        'Oops! algo salio mal, recarga la pagina eh intentalo de nuevo';
+
+      this.contactoService.enviarMail(nombre, email, mensaje).subscribe({
+        next: (response) => {
+          if (response['result'] == 'success')
+            this.mensajeDeRespuesta =
+              'Ey, gracias por el mensaje! me pondre en contacto contigo lo antes posible';
+          this.formContacto.enable();
+          this.submitted = true;
+          this.cargando = false;
+          console.log(this.mensajeDeRespuesta);
+        },
+        error: (error) => {
+          this.formContacto.enable();
+          this.submitted = true;
+          this.cargando = false;
+          console.log(error);
+        },
+      });
+    }
+  }
 
   getMensajeDeError(input: string) {
     if (this.formContacto.controls['email'].hasError('email')) {
       return 'Usa un mail valido!';
     }
 
-    return this.formContacto.controls[input].hasError('required') ? 'Debes ingresar algo!' : '';
+    return this.formContacto.controls[input].hasError('required')
+      ? 'Debes ingresar algo!'
+      : '';
   }
-
-
 }
--- frontend-angular/src/app/services/contacto.service.ts
+++ frontend-angular/src/app/services/contacto.service.ts
@@ -2,16 +2,16 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-const API_URL_CONTACTO = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQeIaeSQ-6htlEosKXcQ6J0yeUNORN8kydTS6SfYkGO0BPwVkqw-BSGX1yDPuP7fRqLUl39la-ryOqJ/pubhtml';
+const API_URL_CONTACTO =
+  'https://script.google.com/macros/s/AKfycbyecKlludpLr3XHhbIDuQultnHQH0ctV-qO0cmGj8Ez8GZnRbiNeBHfmHb60SHwccRH/exec';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ContactoService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  enviarMail(nombre: string, email: string, asunto: string, msg: string): Observable<any> {
+  enviarMail(nombre: string, email: string, msg: string): Observable<any> {
     let formData: any = new FormData();
     formData.append('name', nombre);
     formData.append('email', email);
