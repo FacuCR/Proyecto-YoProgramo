00b2518cc2b430310b39020b7dacb98705a4b814
FacuCR <facucastro302@gmail.com>
2022-05-07 23:28:50 -0300

    servicio de contacto listo

--- frontend-angular/src/app/services/contacto.service.ts
+++ frontend-angular/src/app/services/contacto.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+const API_URL_CONTACTO = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQeIaeSQ-6htlEosKXcQ6J0yeUNORN8kydTS6SfYkGO0BPwVkqw-BSGX1yDPuP7fRqLUl39la-ryOqJ/pubhtml';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactoService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  enviarMail(nombre: string, email: string, asunto: string, msg: string): Observable<any> {
+    let formData: any = new FormData();
+    formData.append('name', nombre);
+    formData.append('email', email);
+    formData.append('message', msg);
+
+    return this.http.post(API_URL_CONTACTO, formData);
+  }
 }
